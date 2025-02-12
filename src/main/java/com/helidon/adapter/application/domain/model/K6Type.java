ceda27f23e79059014e572d18cc579c37d645432
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-02-12 21:05:03 +0100

    Add K6 type implementation

---
+++ src/main/java/com/helidon/adapter/application/domain/model/K6Type.java
@@ -0,0 +1,10 @@
+package com.helidon.adapter.application.domain.model;
+
+import com.helidon.application.domain.model.Type;
+
+public enum K6Type implements Type {
+    TIME,
+    GAUGE,
+    COUNTER,
+    RATE
+}
