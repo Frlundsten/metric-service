75c48438b4f3c2c202ab5434b5dd9e9e10363cb5
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-02-16 22:29:03 +0100

    Add jackson support to webserver

--- src/main/java/com/helidon/adapter/web/HelidonWebserver.java
+++ src/main/java/com/helidon/adapter/web/HelidonWebserver.java
@@ -1,15 +1,24 @@
 package com.helidon.adapter.web;
 
-import com.helidon.adapter.application.k6.in.GetHandler;
-import com.helidon.adapter.application.k6.in.PostHandler;
 import com.helidon.adapter.application.k6.domain.service.GetService;
 import com.helidon.adapter.application.k6.domain.service.PostService;
+import com.helidon.adapter.application.k6.in.GetHandler;
+import com.helidon.adapter.application.k6.in.PostHandler;
 import com.helidon.web.Webserver;
 import io.helidon.config.Config;
+import io.helidon.http.media.MediaContext;
+import io.helidon.http.media.MediaContextConfig;
+import io.helidon.http.media.MediaSupport;
+import io.helidon.http.media.jackson.JacksonSupport;
 import io.helidon.webserver.WebServer;
 import io.helidon.webserver.http.HttpRouting;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HelidonWebserver implements Webserver {
+
+    public static Logger log = LoggerFactory.getLogger(HelidonWebserver.class);
+
     public static void setup() {
         GetService getService = new GetService();
         PostService postService = new PostService();
@@ -20,11 +29,19 @@ public class HelidonWebserver implements Webserver {
         Config config = Config.create();
 
         WebServer
-                .builder()
+                .builder().mediaContext(getMediaContext(config))
                 .config(config.get("server"))
-                .routing(HttpRouting.builder().get(getHandler::handleRequest).post(postHandler::handleRequest))
+                .routing(HttpRouting.builder().register().get(getHandler::handleRequest).post(postHandler::handleRequest))
                 .build()
                 .start();
+
+        log.info("HelidonWebserver started");
+    }
+
+    private static MediaContext getMediaContext(Config config) {
+        MediaSupport jacksonSupport = JacksonSupport.create(config);
+
+        return MediaContextConfig.builder().addMediaSupport(jacksonSupport).build();
     }
 }
 
