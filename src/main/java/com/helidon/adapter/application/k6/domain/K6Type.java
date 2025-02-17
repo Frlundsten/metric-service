ab8e5ad6db1ddc6c128361e5c0e2501b41c23f61
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-02-17 22:00:31 +0100

    Update K6Type enum

--- src/main/java/com/helidon/adapter/application/k6/domain/K6Type.java
+++ src/main/java/com/helidon/adapter/application/k6/domain/K6Type.java
@@ -3,8 +3,8 @@ package com.helidon.adapter.application.k6.domain;
 import com.helidon.application.domain.model.Type;
 
 public enum K6Type implements Type {
-    TIME,
-    GAUGE,
-    COUNTER,
-    RATE
+  TREND,
+  GAUGE,
+  COUNTER,
+  RATE;
 }
