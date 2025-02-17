e0e9bc4aefd1c439f4cea59b2cc87f19e1e8037f
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-02-17 22:01:06 +0100

    Edit gauge and counter value objects name

--- src/main/java/com/helidon/adapter/application/k6/domain/CounterValues.java
+++ src/main/java/com/helidon/adapter/application/k6/domain/CounterValues.java
@@ -2,5 +2,4 @@ package com.helidon.adapter.application.k6.domain;
 
 import com.helidon.application.domain.model.Values;
 
-public record K6GaugeValues(double value, double min, double max) implements Values {
-}
+public record CounterValues(double count, double rate) implements Values {}
--- src/main/java/com/helidon/adapter/application/k6/domain/GaugeValues.java
+++ src/main/java/com/helidon/adapter/application/k6/domain/GaugeValues.java
@@ -2,5 +2,4 @@ package com.helidon.adapter.application.k6.domain;
 
 import com.helidon.application.domain.model.Values;
 
-public record K6CounterValues(double count, double rate) implements Values {
-}
+public record GaugeValues(double value, double min, double max) implements Values {}
