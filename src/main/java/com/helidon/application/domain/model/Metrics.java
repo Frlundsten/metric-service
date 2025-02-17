59afb9e6603c240a78c37df816b2466f0fcac2da
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-02-17 22:03:22 +0100

    Add timestamp on creation

--- src/main/java/com/helidon/application/domain/model/Metrics.java
+++ src/main/java/com/helidon/application/domain/model/Metrics.java
@@ -1,7 +1,11 @@
 package com.helidon.application.domain.model;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
-public record Metrics(String data, Instant timestamp , List<Metric> metricList) {
+public record Metrics(String data, Instant timestamp, List<Metric> metricList) {
+  public Metrics(String data, List<Metric> metricList) {
+    this(data, Instant.now().truncatedTo(ChronoUnit.SECONDS), metricList);
+  }
 }
