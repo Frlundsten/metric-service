783d545edab08b0396e9ec7f8e7dd22abbdbf430
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-02-23 21:47:07 +0100

    Formatting

--- src/main/java/com/helidon/Main.java
+++
@@ -1,9 +0,0 @@
-package com.helidon;
-
-
-import com.helidon.adapter.web.HelidonWebserver;
-
-public class Main {
-    public static void main(String[] args) {
-        HelidonWebserver.setup();    }
-}
--- src/main/java/com/helidon/web/Webserver.java
+++
@@ -1,6 +0,0 @@
-package com.helidon.web;
-
-public interface Webserver {
-    static void setup() {
-    }
-}
