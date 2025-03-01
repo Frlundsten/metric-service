05ab2f593997d184c420a881f7379d4474dda955
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-03-01 21:32:35 +0100

    Use uuidV4 as ID

--- src/main/java/com/helidon/application/domain/model/Metrics.java
+++ src/main/java/com/helidon/application/domain/model/Metrics.java
@@ -3,9 +3,14 @@ package com.helidon.application.domain.model;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.UUID;
 
-public record Metrics(String data, Instant timestamp, List<Metric> metricList) {
+public record Metrics(String id, String data, Instant timestamp, List<Metric> metricList) {
   public Metrics(String data, List<Metric> metricList) {
-    this(data, Instant.now().truncatedTo(ChronoUnit.SECONDS), metricList);
+    this(
+        UUID.randomUUID().toString(),
+        data,
+        Instant.now().truncatedTo(ChronoUnit.SECONDS),
+        metricList);
   }
 }
