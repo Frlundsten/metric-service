2265ffdc99a7ec6a1349268e619b02356b24ab54
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-03-03 21:07:29 +0100

    Add repository insert in same transaction

--- src/main/java/com/helidon/adapter/k6/out/MetricJDBCRepository.java
+++ src/main/java/com/helidon/adapter/k6/out/MetricJDBCRepository.java
@@ -1,9 +1,12 @@
 package com.helidon.adapter.k6.out;
 
+import com.helidon.application.domain.model.Metric;
 import com.helidon.application.domain.model.Metrics;
 import com.helidon.application.port.out.Repository;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.List;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,28 +23,65 @@ public class MetricJDBCRepository implements Repository {
   public void save(Metrics metrics) {
     log.info("Saving metrics {}", metrics);
 
-    String sql = "INSERT INTO metrics VALUES (?, ?::JSON, ?)";
-
     try (var conn = dataSource.getConnection()) {
       conn.setAutoCommit(false);
-      try (var stmt = conn.prepareStatement(sql)) {
-        stmt.setString(1, metrics.id());
-        stmt.setString(2, metrics.data());
-        stmt.setTimestamp(3, Timestamp.from(metrics.timestamp()));
 
-        int rows = stmt.executeUpdate();
-        if (rows != 1) {
-          throw new SQLException("Rows affected: " + rows);
-        }
-      } catch (SQLException e) {
-        conn.rollback();
-        log.error("Rolling back transaction");
-        throw e;
-      }
+      saveMetricsData(conn, metrics);
+      saveMetrics(conn, metrics.id(), metrics.metricList());
+
       conn.commit();
     } catch (SQLException e) {
       log.error("Exception while saving metrics", e);
       throw new RuntimeException("Exception while saving metrics", e);
+    }
+  }
+
+  private void saveMetricsData(Connection conn, Metrics metrics) throws SQLException {
+    String sql = "INSERT INTO metrics VALUES (?, ?::JSON, ?)";
+
+    try (var stmt = conn.prepareStatement(sql)) {
+      stmt.setString(1, metrics.id());
+      stmt.setString(2, metrics.data());
+      stmt.setTimestamp(3, Timestamp.from(metrics.timestamp()));
+
+      int rows = stmt.executeUpdate();
+
+      if (rows != 1) {
+        throw new SQLException("Rows affected: " + rows);
+      }
+
+    } catch (SQLException e) {
+      conn.rollback();
+      log.error("Rolling back transaction");
+      throw e;
+    }
+  }
+
+  private void saveMetrics(Connection conn, String metricsId, List<Metric> metrics)
+      throws SQLException {
+    String sql = "INSERT INTO metric VALUES (?, ?, ?, ?)";
+
+    try (var stmt = conn.prepareStatement(sql)) {
+
+      for (Metric metric : metrics) {
+        stmt.setString(1, metric.id());
+        stmt.setString(2, metric.name());
+        stmt.setString(3, metricsId);
+        stmt.setString(4, metric.type().getType());
+
+        stmt.addBatch();
+      }
+
+      var responseList = stmt.executeBatch();
+
+      if (responseList.length != metrics.size()) {
+        throw new SQLException("Batch update error. Rows affected: " + responseList.length);
+      }
+
+    } catch (SQLException e) {
+      conn.rollback();
+      log.error("Rolling back transaction");
+      throw e;
     }
   }
 
