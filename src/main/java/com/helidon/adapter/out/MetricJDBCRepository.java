74f508e802f6d30a74acbffa22696961b7c54865
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-03-08 12:42:23 +0100

    Use dbclient

--- src/main/java/com/helidon/adapter/out/MetricJDBCRepository.java
+++ src/main/java/com/helidon/adapter/out/MetricJDBCRepository.java
@@ -4,86 +4,91 @@ import com.helidon.application.domain.model.Metric;
 import com.helidon.application.domain.model.Metrics;
 import com.helidon.application.port.out.create.ForPersistingMetrics;
 import com.helidon.application.port.out.manage.ForManagingStoredMetrics;
+import io.helidon.dbclient.DbClient;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.List;
-import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MetricJDBCRepository implements ForPersistingMetrics, ForManagingStoredMetrics {
   public static Logger log = LoggerFactory.getLogger(MetricJDBCRepository.class);
-  private final DataSource dataSource;
+  private final DbClient dbClient;
 
-  public MetricJDBCRepository(DataSource dataSource) {
-    this.dataSource = dataSource;
+  public MetricJDBCRepository(DbClient dbClient) {
+    this.dbClient = dbClient;
   }
 
   @Override
   public void saveMetrics(Metrics metrics) {
     log.debug("Saving metrics {}", metrics);
 
-    try (var conn = dataSource.getConnection()) {
-      conn.setAutoCommit(false);
+    dbClient
+            .transaction()
+            .namedInsert("insertMetrics", metrics.id(), metrics.data(), Timestamp.from(metrics.timestamp()));
 
-      saveMetricsData(conn, metrics);
-      saveMetrics(conn, metrics.id(), metrics.metricList());
-
-      conn.commit();
-    } catch (SQLException e) {
-      log.error("Exception while saving metrics", e);
-      throw new RuntimeException("Exception while saving metrics", e);
-    }
+    //    try (var conn = dataSource.getConnection()) {
+    //      conn.setAutoCommit(false);
+    //
+    //      saveMetricsData(conn, metrics);
+    //      saveMetrics(conn, metrics.id(), metrics.metricList());
+    //
+    //      conn.commit();
+    //    } catch (SQLException e) {
+    //      log.error("Exception while saving metrics", e);
+    //      throw new RuntimeException("Exception while saving metrics", e);
+    //    }
   }
 
   private void saveMetricsData(Connection conn, Metrics metrics) throws SQLException {
-    String sql = "INSERT INTO metrics VALUES (?, ?::JSON, ?)";
-
-    try (var stmt = conn.prepareStatement(sql)) {
-      stmt.setString(1, metrics.id());
-      stmt.setString(2, metrics.data());
-      stmt.setTimestamp(3, Timestamp.from(metrics.timestamp()));
-
-      int rows = stmt.executeUpdate();
-
-      if (rows != 1) {
-        throw new SQLException("Rows affected: " + rows);
-      }
-
-    } catch (SQLException e) {
-      conn.rollback();
-      log.error("Rolling back transaction");
-      throw e;
-    }
+    //    String sql = "INSERT INTO metrics VALUES (?, ?::JSON, ?)";
+    //
+    //    try (var stmt = conn.prepareStatement(sql)) {
+    //      stmt.setString(1, metrics.id());
+    //      stmt.setString(2, metrics.data());
+    //      stmt.setTimestamp(3, Timestamp.from(metrics.timestamp()));
+    //
+    //      int rows = stmt.executeUpdate();
+    //
+    //      if (rows != 1) {
+    //        throw new SQLException("Rows affected: " + rows);
+    //      }
+    //
+    //    } catch (SQLException e) {
+    //      conn.rollback();
+    //      log.error("Rolling back transaction");
+    //      throw e;
+    //    }
   }
 
   private void saveMetrics(Connection conn, String metricsId, List<Metric> metrics)
       throws SQLException {
-    String sql = "INSERT INTO metric VALUES (?, ?, ?, ?)";
-
-    try (var stmt = conn.prepareStatement(sql)) {
-
-      for (Metric metric : metrics) {
-        stmt.setString(1, metric.id());
-        stmt.setString(2, metric.name());
-        stmt.setString(3, metricsId);
-        stmt.setString(4, metric.type().getType());
-
-        stmt.addBatch();
-      }
-
-      var responseList = stmt.executeBatch();
-
-      if (responseList.length != metrics.size()) {
-        throw new SQLException("Batch update error. Rows affected: " + responseList.length);
-      }
-
-    } catch (SQLException e) {
-      conn.rollback();
-      log.error("Rolling back transaction");
-      throw e;
-    }
+    //    String sql = "INSERT INTO metric VALUES (?, ?, ?, ?)";
+    //
+    //    try (var stmt = conn.prepareStatement(sql)) {
+    //
+    //      for (Metric metric : metrics) {
+    //        stmt.setString(1, metric.id());
+    //        stmt.setString(2, metric.name());
+    //        stmt.setString(3, metricsId);
+    //        stmt.setString(4, metric.type().getType());
+    //
+    //        stmt.addBatch();
+    //      }
+    //
+    //      var responseList = stmt.executeBatch();
+    //
+    //      if (responseList.length != metrics.size()) {
+    //        throw new SQLException("Batch update error. Rows affected: " + responseList.length);
+    //      }
+    //
+    //    } catch (SQLException e) {
+    //      conn.rollback();
+    //      log.error("Rolling back transaction");
+    //      throw e;
+    //    }
   }
 
   @Override
--- src/main/java/com/helidon/startup/HelidonWebserver.java
+++ src/main/java/com/helidon/startup/HelidonWebserver.java
@@ -6,14 +6,15 @@ import com.helidon.adapter.in.rest.GetMetricsHandler;
 import com.helidon.adapter.in.rest.Mapper;
 import com.helidon.adapter.out.MetricJDBCRepository;
 import com.helidon.application.domain.service.MetricService;
+import io.helidon.common.concurrency.limits.Limit;
 import io.helidon.config.Config;
+import io.helidon.dbclient.DbClient;
 import io.helidon.http.media.MediaContext;
 import io.helidon.http.media.MediaContextConfig;
 import io.helidon.http.media.MediaSupport;
 import io.helidon.http.media.jackson.JacksonSupport;
 import io.helidon.webserver.WebServer;
 import io.helidon.webserver.http.HttpRouting;
-import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,20 +24,19 @@ public class HelidonWebserver {
 
   public static void setup() {
     Mapper mapper = new Mapper();
-    DataSource dataSource =
-        DataSourceInstance.getDataSource(
-            "jdbc:postgresql://localhost:5432/helidon", "user", "password");
 
-    MetricJDBCRepository repository = new MetricJDBCRepository(dataSource);
+    Config config = Config.create();
+    Config dbConfig =  config.get("db");
+
+    DbClient dbClient = DbClient.create(dbConfig);
+
+    MetricJDBCRepository repository = new MetricJDBCRepository(dbClient);
     MetricService metricService = new MetricService(repository, repository);
     CreateMetricsHandler createMetricsHandler = new CreateMetricsHandler(metricService, mapper);
     GetMetricsHandler getMetricsHandler = new GetMetricsHandler(metricService, mapper);
 
     DelegatingService delegatingService =
         new DelegatingService(createMetricsHandler, getMetricsHandler);
-
-    Config config = Config.create();
-
     WebServer.builder()
         .mediaContext(getMediaContext(config))
         .config(config.get("server"))
