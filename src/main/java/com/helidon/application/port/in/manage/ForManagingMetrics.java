ee569db3d8b10b2989dc030270b51ff061373a00
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-03-09 21:00:02 +0100

    Add documentation to methods

--- src/main/java/com/helidon/application/port/in/manage/ForManagingMetrics.java
+++ src/main/java/com/helidon/application/port/in/manage/ForManagingMetrics.java
@@ -1,7 +1,25 @@
 package com.helidon.application.port.in.manage;
 
 import com.helidon.application.domain.model.Metrics;
+import java.time.Instant;
+import java.util.List;
 
 public interface ForManagingMetrics {
+
+  /**
+   * Get full metrics from an id.
+   *
+   * @param id
+   * @return Metrics object with provided id
+   */
   Metrics getMetrics(String id);
+
+  /**
+   * Get metrics objects from a timespan.
+   *
+   * @param start Inclusive
+   * @param end Exclusive
+   * @return List of Metrics objects
+   */
+  List<Metrics> getBetweenDates(Instant start, Instant end);
 }
--- src/main/java/com/helidon/application/port/out/manage/ForManagingStoredMetrics.java
+++ src/main/java/com/helidon/application/port/out/manage/ForManagingStoredMetrics.java
@@ -2,6 +2,24 @@ package com.helidon.application.port.out.manage;
 
 import com.helidon.application.domain.model.Metrics;
 
+import java.time.Instant;
+import java.util.List;
+
 public interface ForManagingStoredMetrics {
+  /**
+   * Get full metrics from an id.
+   *
+   * @param id
+   * @return Metrics object with provided id
+   */
   Metrics get(String id);
+
+  /**
+   * Get metrics objects from a timespan.
+   *
+   * @param start Inclusive
+   * @param end Exclusive
+   * @return List of Metrics objects
+   */
+  List<Metrics> getBetweenDates(Instant start, Instant end);
 }
