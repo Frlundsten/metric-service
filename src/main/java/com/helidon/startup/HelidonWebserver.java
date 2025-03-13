cbbc11ea504f444e12b3cf8db3ed88a84625b974
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-03-13 21:18:27 +0100

    Refactor to keep clean

--- src/main/java/com/helidon/startup/HelidonWebserver.java
+++ src/main/java/com/helidon/startup/HelidonWebserver.java
@@ -5,21 +5,13 @@ import com.helidon.adapter.in.rest.DelegatingService;
 import com.helidon.adapter.in.rest.GetMetricsHandler;
 import com.helidon.adapter.in.rest.Mapper;
 import com.helidon.adapter.out.MetricJDBCRepository;
-import com.helidon.application.ObjectMapperFactory;
 import com.helidon.application.domain.service.MetricService;
 import io.helidon.config.Config;
-import io.helidon.http.media.MediaContext;
-import io.helidon.http.media.MediaContextConfig;
-import io.helidon.http.media.jackson.JacksonSupport;
 import io.helidon.webserver.WebServer;
 import io.helidon.webserver.http.HttpRouting;
 import javax.sql.DataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class HelidonWebserver {
-
-  public static Logger LOG = LoggerFactory.getLogger(HelidonWebserver.class);
 
   public static void setup() {
     Mapper mapper = new Mapper();
@@ -38,18 +30,9 @@ public class HelidonWebserver {
     Config config = Config.create();
 
     WebServer.builder()
-        .mediaContext(getJackson())
         .config(config.get("server"))
         .routing(HttpRouting.builder().register("/metrics", delegatingService))
         .build()
         .start();
-
-    LOG.debug("HelidonWebserver started");
-  }
-
-  private static MediaContext getJackson() {
-    return MediaContextConfig.builder()
-        .addMediaSupport(JacksonSupport.create(ObjectMapperFactory.create()))
-        .build();
   }
 }
