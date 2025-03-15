051e22dcfb0f7ea0514c7c0fe15fdbd14782704e
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-03-15 22:04:58 +0100

    Add testscope

---
+++ src/test/java/com/helidon/ProvideScope.java
@@ -0,0 +1,10 @@
+package com.helidon;
+
+import com.helidon.application.domain.model.RepositoryId;
+
+// Provide scoped value for tests
+public class ProvideScope {
+  public static void withScope(Runnable scopeForTest) {
+    ScopedValue.where(RepositoryId.REPOSITORY_ID, new RepositoryId("test-repo")).run(scopeForTest);
+  }
+}
