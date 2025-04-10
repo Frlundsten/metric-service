b30386e77283dd0c9266ff882808a80bcc018c65
Fredrik Lundsten <fredrik.lundsten@extendaretail.com>
2025-04-10 15:11:11 +0200

    Remove enum validation

--- src/main/java/com/helidon/application/domain/model/MetricName.java
+++ src/main/java/com/helidon/application/domain/model/MetricName.java
@@ -1,17 +1,10 @@
 package com.helidon.application.domain.model;
-
-import com.helidon.application.domain.WantedK6Metrics;
 import com.helidon.exception.MetricNameValidationException;
 
 public record MetricName(String value) {
     public MetricName {
         if (value == null || value.isBlank()) {
             throw new MetricNameValidationException("Metric name cannot be null or empty");
-        }
-        try {
-            WantedK6Metrics.valueOf(value.toUpperCase());
-        } catch (IllegalArgumentException e) {
-            throw new MetricNameValidationException("Not a valid metric name " + value);
         }
     }
 }
