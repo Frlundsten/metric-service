a330a30c7bb356f5e3c9faf2d58294d7054d1bdc
Fredrik Lundsten <fredrik.lundsten@extendaretail.com>
2025-04-10 15:08:40 +0200

    Add scoped value and implement method

--- src/main/java/com/helidon/adapter/in/rest/GetMetricsHandler.java
+++ src/main/java/com/helidon/adapter/in/rest/GetMetricsHandler.java
@@ -1,14 +1,14 @@
 package com.helidon.adapter.in.rest;
 
-import com.helidon.adapter.in.rest.dto.MetricsRequestDTO;
+import com.helidon.adapter.in.rest.dto.MetricsResponseDTO;
+import com.helidon.application.RepositoryId;
 import com.helidon.application.port.in.manage.ForManagingMetrics;
-import com.helidon.util.Mapper;
 import io.helidon.http.HeaderNames;
 import io.helidon.webserver.http.Handler;
 import io.helidon.webserver.http.ServerRequest;
 import io.helidon.webserver.http.ServerResponse;
 import java.time.Instant;
-import java.util.Map;
+import java.util.List;
 import java.util.NoSuchElementException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,40 +16,36 @@ import org.slf4j.LoggerFactory;
 public class GetMetricsHandler implements Handler {
   public static Logger LOG = LoggerFactory.getLogger(GetMetricsHandler.class);
   private final ForManagingMetrics forManagingMetrics;
-  private final Mapper mapper;
 
-  public GetMetricsHandler(ForManagingMetrics forManagingMetrics, Mapper mapper) {
+  public GetMetricsHandler(ForManagingMetrics forManagingMetrics) {
     this.forManagingMetrics = forManagingMetrics;
-    this.mapper = mapper;
   }
 
   @Override
   public void handle(ServerRequest req, ServerResponse res) {
     LOG.debug("Processing a GET request..");
-    try {
-      var headerValue = req.headers().get(HeaderNames.create("repository-id"));
-      var id = headerValue.get();
-      System.out.println(RepositoryId.getScopedValue());
-      Map<String, Object> request = req.content().as(Map.class);
-
-      var fromDate = request.get("from");
-      var toDate = request.get("to");
-      if (fromDate != null) {
-        LOG.debug("Found from body: {}", fromDate);
-
-        var from = Instant.parse((CharSequence) fromDate);
-        var to = Instant.parse((CharSequence) toDate);
-
-        var response = forManagingMetrics.getBetweenDates(from, to);
-
-        res.status(200).send(response);
-      }
-    } catch (NoSuchElementException e) {
-      res.status(400).send(e);
-    }
+    var repoIdValue = req.headers().get(HeaderNames.create("Repository-Id"));
+    RepositoryId repositoryId = new RepositoryId(repoIdValue.values());
+    ScopedValue.where(RepositoryId.REPOSITORY_ID, repositoryId)
+        .run(
+            () -> {
+              try {
+                var fromDate = req.query().get("from");
+                var toDate = req.query().get("to");
+                LOG.debug("Processing a GET request..WRFGFAWEFFSFS");
+                var response = handleRequest(fromDate, toDate);
+                res.status(200).send(response);
+              } catch (NoSuchElementException e) {
+                res.status(400).send(e);
+              }
+            });
   }
 
-  private MetricsRequestDTO handleRequest(String id) {
-    return null;
+  private List<MetricsResponseDTO> handleRequest(String fromDate, String toDate) {
+    LOG.debug("Get metrics created between {} and {}", fromDate, toDate);
+    var from = Instant.parse(fromDate);
+    var to = Instant.parse(toDate);
+    var metricsList = forManagingMetrics.getBetweenDates(from, to);
+    return metricsList.stream().map(MetricsResponseDTO::from).toList();
   }
 }
