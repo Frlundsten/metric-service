8d2cf78e2d2cb24b6c79eac5d29bb019de58033e
Fredrik Lundsten <fredrik.lundsten@extendaretail.com>
2025-04-10 15:08:16 +0200

    Remove unused mapper from handler

--- src/main/java/com/helidon/startup/Main.java
+++ src/main/java/com/helidon/startup/Main.java
@@ -3,9 +3,9 @@ package com.helidon.startup;
 import com.helidon.adapter.in.rest.CreateMetricsHandler;
 import com.helidon.adapter.in.rest.DelegatingService;
 import com.helidon.adapter.in.rest.GetMetricsHandler;
-import com.helidon.util.Mapper;
 import com.helidon.adapter.out.MetricJDBCRepository;
 import com.helidon.application.domain.service.MetricService;
+import com.helidon.util.Mapper;
 import javax.sql.DataSource;
 
 public class Main {
@@ -15,10 +15,10 @@ public class Main {
         DataSourceInstance.getDataSource(
             "jdbc:postgresql://localhost:5432/helidon", "user", "password");
 
-    MetricJDBCRepository repository = new MetricJDBCRepository(dataSource,mapper);
+    MetricJDBCRepository repository = new MetricJDBCRepository(dataSource, mapper);
     MetricService metricService = new MetricService(repository, repository);
     CreateMetricsHandler createMetricsHandler = new CreateMetricsHandler(metricService, mapper);
-    GetMetricsHandler getMetricsHandler = new GetMetricsHandler(metricService, mapper);
+    GetMetricsHandler getMetricsHandler = new GetMetricsHandler(metricService);
 
     DelegatingService delegatingService =
         new DelegatingService(createMetricsHandler, getMetricsHandler);
