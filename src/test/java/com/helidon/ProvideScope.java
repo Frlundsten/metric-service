c0cde5261ccf02691b3f003c7a8d20f549f0b466
Fredrik Lundsten <fredrik.lundsten@extendaretail.com>
2025-04-23 13:09:44 +0200

    Add callable scope

--- src/test/java/com/helidon/ProvideScope.java
+++ src/test/java/com/helidon/ProvideScope.java
@@ -1,10 +1,15 @@
 package com.helidon;
 
 import com.helidon.adapter.RepositoryId;
+import com.helidon.exception.ProvidedScopeException;
+
 
 // Provide scoped value for tests
 public class ProvideScope {
   public static void withScope(Runnable scopeForTest) {
     ScopedValue.where(RepositoryId.REPOSITORY_ID, new RepositoryId("test-repo")).run(scopeForTest);
+  }
+  public static String withScope(ScopedValue.CallableOp<String, ProvidedScopeException> scopeForTest) {
+    return ScopedValue.where(RepositoryId.REPOSITORY_ID, new RepositoryId("test-repo")).call(scopeForTest);
   }
 }
