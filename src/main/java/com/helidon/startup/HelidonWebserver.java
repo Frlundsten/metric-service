1b60b2e6e95ec1e181d7e976d26f0a9e6e8a289c
Fredrik Lundsten <fredrik.lundsten@extendaretail.com>
2025-04-24 11:47:41 +0200

    Refactor

--- src/main/java/com/helidon/startup/HelidonWebserver.java
+++ src/main/java/com/helidon/startup/HelidonWebserver.java
@@ -5,15 +5,9 @@ import com.helidon.adapter.in.rest.DelegatingService;
 import com.helidon.adapter.in.rest.GetMetricsHandler;
 import com.helidon.adapter.out.MetricJDBCRepository;
 import com.helidon.application.domain.service.MetricService;
-import com.helidon.util.Mapper;
+import com.helidon.adapter.Mapper;
 import io.helidon.config.Config;
 import io.helidon.dbclient.DbClient;
-import io.helidon.dbclient.DbMapper;
-import io.helidon.dbclient.spi.DbClientBuilder;
-import io.helidon.http.media.MediaContext;
-import io.helidon.http.media.MediaContextConfig;
-import io.helidon.http.media.MediaSupport;
-import io.helidon.http.media.jackson.JacksonSupport;
 import io.helidon.webserver.WebServer;
 import io.helidon.webserver.http.HttpRouting;
 import org.slf4j.Logger;
@@ -21,7 +15,9 @@ import org.slf4j.LoggerFactory;
 
 public class HelidonWebserver {
 
-  public static Logger LOG = LoggerFactory.getLogger(HelidonWebserver.class);
+  public static final Logger LOG = LoggerFactory.getLogger(HelidonWebserver.class);
+
+  private HelidonWebserver() {}
 
   public static void setup() {
     Config config = Config.create();
@@ -37,17 +33,11 @@ public class HelidonWebserver {
         new DelegatingService(createMetricsHandler, getMetricsHandler);
 
     WebServer.builder()
-        .mediaContext(getMediaContext(config))
         .config(config.get("server"))
         .routing(HttpRouting.builder().register("/metrics", delegatingService))
         .build()
         .start();
 
     LOG.debug("HelidonWebserver started");
-  }
-
-  private static MediaContext getMediaContext(Config config) {
-    MediaSupport jacksonSupport = JacksonSupport.create(config);
-    return MediaContextConfig.builder().addMediaSupport(jacksonSupport).build();
   }
 }
