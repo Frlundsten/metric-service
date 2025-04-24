987a0ef9fd390af49d09771f965d174b49848c5c
Fredrik Lundsten <fredrik.lundsten@extendaretail.com>
2025-04-24 11:51:21 +0200

    Add dto method

--- src/main/java/com/helidon/adapter/in/rest/dto/TrendValuesDTO.java
+++ src/main/java/com/helidon/adapter/in/rest/dto/TrendValuesDTO.java
@@ -2,6 +2,7 @@ package com.helidon.adapter.in.rest.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.helidon.application.domain.model.TrendValues;
+import com.helidon.application.domain.model.Values;
 
 public record TrendValuesDTO(
     double max,
@@ -15,5 +16,12 @@ public record TrendValuesDTO(
   @Override
   public TrendValues toDomain() {
     return new TrendValues(max(), min(), avg(), med(), p95(), p90());
+  }
+
+  @Override
+  public ValuesDTO toDTO(Values domain) {
+    var values = (TrendValues) domain;
+    return new TrendValuesDTO(
+        values.max(), values.min(), values.avg(), values.med(), values.p95(), values.p90());
   }
 }
