c3b46f6ef72cc230cc3ec678a03ca525a539236c
Fredrik Lundsten <fredrik.lundsten@extendaretail.com>
2025-05-09 15:52:10 +0200

    Validate value

--- src/main/java/com/helidon/adapter/common/RepositoryId.java
+++ src/main/java/com/helidon/adapter/common/RepositoryId.java
@@ -6,10 +6,10 @@ public record RepositoryId(String value) {
   public static final ScopedValue<RepositoryId> REPOSITORY_ID = ScopedValue.newInstance();
 
   public RepositoryId {
-    Objects.requireNonNull(value, "Repository id cannot be null");
     if (value.isEmpty()) {
       throw new IllegalArgumentException("Repository id cannot be empty");
     }
+    Objects.requireNonNull(value, "Repository id cannot be null");
   }
 
   public static RepositoryId getScopedValue() {
