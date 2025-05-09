c2cf71e5c46f1bea6e9c9a0d758b79a7a4f646cb
Fredrik Lundsten <fredrik.lundsten@extendaretail.com>
2025-05-09 15:49:13 +0200

    Extract methods

--- src/main/java/com/helidon/startup/HelidonWebserver.java
+++ src/main/java/com/helidon/startup/HelidonWebserver.java
@@ -1,19 +1,19 @@
 package com.helidon.startup;
 
+import com.helidon.adapter.common.Mapper;
+import com.helidon.adapter.common.MetricReportTypeMapperProvider;
 import com.helidon.adapter.in.rest.CreateMetricsHandler;
 import com.helidon.adapter.in.rest.DelegatingService;
+import com.helidon.adapter.in.rest.RecentReportshandler;
 import com.helidon.adapter.in.rest.ReportTimespanHandler;
-import com.helidon.adapter.in.rest.RecentReportsHandler;
 import com.helidon.adapter.out.MetricJDBCRepository;
 import com.helidon.application.domain.service.MetricService;
-import com.helidon.adapter.Mapper;
 import io.helidon.config.Config;
 import io.helidon.dbclient.DbClient;
 import io.helidon.webserver.WebServer;
 import io.helidon.webserver.http.HttpRouting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 
 public class HelidonWebserver {
 
@@ -24,23 +24,34 @@ public class HelidonWebserver {
   public static void setup() {
     Config config = Config.create();
     Mapper mapper = new Mapper();
-    DbClient dbClient = DbClient.create(config.get("db"));
 
-    MetricJDBCRepository repository = new MetricJDBCRepository(dbClient);
-    MetricService metricService = new MetricService(repository, repository);
-    CreateMetricsHandler createMetricsHandler = new CreateMetricsHandler(metricService, mapper);
-    ReportTimespanHandler reportTimespanHandler = new ReportTimespanHandler(metricService);
-    RecentReportsHandler recentReportshandler = new RecentReportsHandler(metricService);
+    DbClient client =
+        DbClient.builder(config.get("db"))
+            .mapperProvider(new MetricReportTypeMapperProvider())
+            .build();
 
-    DelegatingService delegatingService =
-        new DelegatingService(createMetricsHandler, reportTimespanHandler,recentReportshandler);
+    DelegatingService delegatingService = getDelegatingService(client, mapper);
 
     WebServer.builder()
         .config(config.get("server"))
-        .routing(HttpRouting.builder().register("/metrics", delegatingService))
+        .routing(getRouting(delegatingService))
         .build()
         .start();
 
     LOG.debug("HelidonWebserver started");
+  }
+
+  private static HttpRouting.Builder getRouting(DelegatingService delegatingService) {
+    return HttpRouting.builder().register("/metrics", delegatingService);
+  }
+
+  private static DelegatingService getDelegatingService(DbClient client, Mapper mapper) {
+    MetricJDBCRepository repository = new MetricJDBCRepository(client);
+    MetricService metricService = new MetricService(repository, repository);
+    CreateMetricsHandler createMetricsHandler = new CreateMetricsHandler(metricService, mapper);
+    ReportTimespanHandler reportTimespanHandler = new ReportTimespanHandler(metricService);
+    RecentReportshandler recentReportshandler = new RecentReportshandler(metricService);
+
+      return new DelegatingService(createMetricsHandler, reportTimespanHandler,recentReportshandler);
   }
 }
