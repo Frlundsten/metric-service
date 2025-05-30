5c7e87caa2f1900bfc5762b4c7a0fb5b85fe61bd
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-05-30 13:24:22 +0200

    Fix formatting

--- acceptance/k6/script.js
+++ acceptance/k6/script.js
@@ -1,11 +1,11 @@
 import http from 'k6/http';
-import { sleep, check } from 'k6';
+import {sleep} from 'k6';
 
 export const options = {
     stages: [
-        { duration: '3s', target: 10 },
-        { duration: '4s', target: 50 },
-        { duration: '3s', target: 0 },
+        {duration: '3s', target: 10},
+        {duration: '4s', target: 50},
+        {duration: '3s', target: 0},
     ],
     thresholds: {
         http_req_duration: ['p(95)<200'], // 95% of requests should complete below 200ms
@@ -20,18 +20,22 @@ export default function () {
         'Repository-Id': 'repo-1'
     };
 
-    const res = http.get(url, { headers });
+    const res = http.get(url, {headers});
 
     console.log(`Request status: ${res.status}`);
 
     sleep(1);
 }
 
-
 export function handleSummary(data) {
 //         console.log(JSON.stringify(data));
 
-        http.post('http://localhost:8080/metrics', JSON.stringify(data),{ headers: { 'Content-Type': 'application/json' , 'Repository-Id': 'fredd'} });
+    http.post('http://localhost:8080/metrics', JSON.stringify(data), {
+        headers: {
+            'Content-Type': 'application/json',
+            'Repository-Id': 'fredd'
+        }
+    });
     return {
         // 'summary.json': JSON.stringify(data),
     };
