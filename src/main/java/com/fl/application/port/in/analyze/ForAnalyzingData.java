719967e175f9a493585c553c54075d29b4823ff0
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-06-03 14:39:53 +0200

    Add analyze ports

--- src/main/java/com/fl/application/port/in/analyze/ForAnalyzingData.java
+++ src/main/java/com/fl/application/port/in/analyze/ForAnalyzingData.java
@@ -1,4 +1,10 @@
 package com.fl.application.port.in.analyze;
 
+import com.fl.application.domain.model.MetricReport;
+
+import java.util.List;
+
 public interface ForAnalyzingData {
+    String analyzeData(List<MetricReport> requests);
+    String analyzeRecentRuns();
 }
